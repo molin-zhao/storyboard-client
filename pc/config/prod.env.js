9a4ddc1d238fc8b6a372941a4e7a3433af4f3484
molin <molinz@molins-MacBook-Pro.local>
2020-04-28 23:23:30 +0800

    client

---
+++ pc/config/prod.env.js
@@ -0,0 +1,9 @@
+"use strict";
+module.exports = {
+  NODE_ENV: '"production"',
+  API_HOST: '"https://api.cokework.com"',
+  PASSPORT_HOST: '"https://passport.cokework.com"',
+  DFS_HOST: '"https://dfs.cokework.com"',
+  SOCKET_HOST: '"https://socket.cokework.com"',
+  COMPUTE_HOST: '"https://compute.cokework.com"'
+};
